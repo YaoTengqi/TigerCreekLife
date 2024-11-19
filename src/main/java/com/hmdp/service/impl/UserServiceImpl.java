403479f8aa5e2d680f0fe66555de376b15ec03f5
YaoTengqi <425045233@qq.com>
2024-11-19 19:53:29 +0800

    完成发送短信验证码

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Random;
 
 /**
  * <p>
@@ -17,4 +23,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1. 验证手机号
+        if (!RegexUtils.isPhoneInvalid(phone)) {
+            // 2. 不符合则报错
+            return Result.fail("The phone number is invalid");
+        }
+        // 3. 符合则生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        // 4. 验证码保存到session
+        session.setAttribute("code", code);
+        // 5. 发送验证码
+        log.debug("send code: " + code + " successfully!");
+        // 6. 返回状态
+        return Result.ok();
+    }
 }
