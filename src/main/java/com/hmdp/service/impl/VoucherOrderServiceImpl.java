23cfff6545b8f8a937b2a2ee0a5da0ec3d33e9d3
Maitheredflower <425045233@qq.com>
2024-12-19 21:17:20 +0800

    将手搓锁改为使用redisson获取锁

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -10,6 +10,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.RedisLock;
 import com.hmdp.utils.UserHolder;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -40,6 +42,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
     @Override
     public Result seckillVoucher(Long voucherId) {
         // 1. 查询优惠券
@@ -62,8 +67,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
         Long userId = UserHolder.getUser().getId();
-        RedisLock redisLock = new RedisLock("order:" + userId, stringRedisTemplate);
-        boolean isLock = redisLock.tryLock(1200);
+//        RedisLock redisLock = new RedisLock("order:" + userId, stringRedisTemplate);
+        RLock redisLock = redissonClient.getLock("order:" + userId);
+        boolean isLock = redisLock.tryLock();
         if (!isLock) {
             // 未能获取锁
             return Result.fail("您已买过此优惠券!");
