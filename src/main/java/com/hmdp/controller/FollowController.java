ee8410ca598f0770089d1c7dec8694cd2b0ff9e4
Maitheredflower <425045233@qq.com>
2024-12-26 10:26:08 +0800

    完成关注和取关

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,13 +1,15 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 虎哥
@@ -16,5 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    @Resource
+    private IFollowService followService;
 
+    @PutMapping("/{id}/{isFollowed}")
+    public Result follow(@PathVariable("id") long id, @PathVariable("isFollowed") boolean isFollowed) {
+        return followService.follow(id, isFollowed);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") long id) {
+        return followService.isFollow(id);
+    }
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,17 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +20,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    /**
+     * 进行关注 or 取关
+     *
+     * @param followUserId
+     * @param isFollowed
+     * @return
+     */
+    @Override
+    public Result follow(long followUserId, boolean isFollowed) {
+        // 获取当前登录的用户信息
+        Long userId = UserHolder.getUser().getId();
+        // 判断是关注还是取关
+        if (isFollowed) {
+            // 关注
+            Follow newFollow = new Follow();
+            newFollow.setFollowUserId(followUserId);
+            newFollow.setUserId(userId);
+            save(newFollow);// 存入数据库中
+        } else {
+            // 取关 delete from tb_follow where user_id = ? and follow_user_id = ?
+            remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", followUserId));
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(long followUserId) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query()
+                .eq("user_id", userId)
+                .eq("follow_user_id", followUserId)
+                .count();
+        return Result.ok(count > 0);
+    }
 }
